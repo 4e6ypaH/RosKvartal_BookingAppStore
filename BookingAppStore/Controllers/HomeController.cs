f74fbc56df2257a10e80cc34b4e35990dd740768
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-11-23 19:59:38 +0300

    lessons 1-7

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookingAppStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,30 @@ namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            var books = db.Books;
+            ViewBag.Books = books;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {           
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public String Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            //добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            //сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
         }
 
         public ActionResult About()
