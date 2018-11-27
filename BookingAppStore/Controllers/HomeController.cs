fdb4faa2bf4ee252a819daccfec667f1ce5a8a88
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-11-27 23:57:32 +0300

    lesson  17

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -13,8 +13,14 @@ namespace BookingAppStore.Controllers
         public ActionResult Index()
         {
             var books = db.Books;
-            ViewBag.Books = books;
-            return View();
+            //ViewBag.Books = books;
+            return View(books);
+        }
+
+        public ActionResult BookIndex()
+        {
+            var books = db.Books;
+            return View(books);
         }
 
         [HttpGet]
